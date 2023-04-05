b1f6838e376e7dcaabf799b97bac0d078cc59b71
irenk <kuzmina.iren.v@gmail.com>
2023-04-05 17:09:30 +0200

    add style for Info remove infoBg

--- src/mocks/info/data.base.info.js
+++ src/mocks/info/data.base.info.js
@@ -1,5 +1,10 @@
+import infoBg from '../../assets/imgs/investment-property/investment-property.png'
+import infoBgMobile from '../../assets/imgs/investment-property/investment-property-mobile.png'
+
 export const info = [
     {
+        infoBg: infoBg,
+        infoBgMobile: infoBgMobile,
         title: 'Investment property',
         text: 'Buying a luxury investment property is one of the more profitable ways of maximizing profits from your savings.',
         btnText: 'More'
